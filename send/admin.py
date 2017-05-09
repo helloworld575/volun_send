12a344e047187de6eed2486e9062bd2b87760b23
Zhichen Li <zhichenli6@gmail.com>
2017-05-09 08:23:21 -0700

    add admin page

--- send/admin.py
+++ send/admin.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from django.db import models
+from django import forms
+from .models import StudentUser,TeacherUser,OrderForm
+
+# class StudentUserAdmin(admin.ModelAdmin):
+#     list_display = ('')
+admin.site.register(StudentUser)
+admin.site.register(TeacherUser)
+admin.site.register(OrderForm)
--- send/models.py
+++ send/models.py
@@ -31,19 +31,23 @@ class StudentUser(models.Model):
     gender = models.CharField(max_length=2,choices=GENDER_CHOICES)
     grade = models.CharField(max_length=5,choices = GRADE_CHOICES)
     Institute = models.CharField(max_length=50)
-    sum_volun_time = models.IntegerField()
-    bad_record = models.IntegerField()
+    sum_volun_time = models.IntegerField(default=0)
+    bad_record = models.IntegerField(default=0)
 
 
     def __str__(self):
         return self.user.username
 
 class TeacherUser(models.Model):
+    SENDORGET = (
+        ('send','send'),
+        ('get','get'),
+    )
     user=models.OneToOneField(User)
-    send_or_get = models.BooleanField() #true send
+    send_or_get = models.CharField(max_length=5,choices = SENDORGET)
     address = models.EmailField(max_length=256)
     phone_number = models.CharField(max_length=20)
-    sum_order_num = models.IntegerField()
+    sum_order_num = models.IntegerField(default=0)
 
     def __str__(self):
         return self.user.username
@@ -52,7 +56,17 @@ class TeacherUser(models.Model):
 class OrderForm(models.Model):
     first_order = -1
 
-    send_people = models.ManyToManyField(TeacherUser)
+    SLOWORFAST = (
+        ('0','slow'),
+        ('1','fast'),
+    )
+
+    ORDERCASE = (
+        ('0','without_get'),
+        ('1','on_the_way'),
+        ('2','has_send'),
+    )
+    teachers = models.ManyToManyField(TeacherUser)
     # get_people = models.ForeignKey(TeacherUser)
     send_student = models.ForeignKey(StudentUser)
 
@@ -63,7 +77,7 @@ class OrderForm(models.Model):
 
     order_number=models.CharField(max_length=15)       #YYYYMMDDIIIIIRR
 
-    volunteer_time = models.IntegerField()
+    volunteer_time = models.IntegerField(default =0)
 
     latest_get_time = models.DateTimeField()
     get_order_time = models.DateTimeField()
@@ -71,11 +85,11 @@ class OrderForm(models.Model):
     actual_send_time = models.DateTimeField()
     pub_time = models.DateTimeField(auto_now=True)
 
-    slow_or_fast = models.BooleanField(default = False)#slow in default
+    slow_or_fast = models.CharField(max_length=5,choices=SLOWORFAST)
 
     other_import = models.TextField()
 
-    order_case = models.CharField(max_length=5)     # 1-order without get 2-order on the way 2 order has sent
+    order_case = models.CharField(max_length=20,choices = ORDERCASE)     # 1-order without get 2-order on the way 2 order has sent
     # order_without_get = models.ForeignKey(TeacherUser)
     # order_on_the_way = models.ForeignKey(TeacherUser)
     # order_has_get = models.ForeignKey(TeacherUser)
